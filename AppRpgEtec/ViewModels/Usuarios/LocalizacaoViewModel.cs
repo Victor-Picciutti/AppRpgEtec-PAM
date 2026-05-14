a1d2fa8861624dbe08a07f5f73b79e52b267e2a3
Victor-Picciutti <victorpicciuttidossantos@gmail.com>
2026-05-13 22:01:56 -0300

    Gps usando API google

---
+++ AppRpgEtec/ViewModels/Usuarios/LocalizacaoViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    using Map = Microsoft.Maui.Controls.Maps.Map;
+    public class LocalizacaoViewModel : BaseViewModel
+    {
+        private Map meuMapa;
+
+        public Map MeuMapa
+        {
+            get => meuMapa;
+            set
+            {
+                if (value != null)
+                {
+                    meuMapa = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public async void InicializarMapa()
+        {
+            try
+            {
+                Location location = new Location(-23.5200241d, -46.596498d);
+                Pin pinEtec = new Pin()
+                {
+                    Type = PinType.Place,
+                    Label = "Etec Horácio",
+                    Address = "Rua alcantara, 113, vila guilherme",
+                    Location = location
+                };
+
+                Map map = new Map();
+                MapSpan mapSpan = MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(5));
+                map.Pins.Add(pinEtec);
+                map.MoveToRegion(mapSpan);
+
+                MeuMapa = map;       
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+
+    }
+}
